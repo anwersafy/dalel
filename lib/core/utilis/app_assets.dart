62f91051da90ba0d4cfb494f47de0a8d41a7c2b6
Anwar Safy <anwarsafy15@gmail.com>
2024-01-14 17:46:46 +0200

    add assests and colors

---
+++ lib/core/utilis/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class  AppAssets {
+  static const String onBoarding1 = 'assets/images/onboarding1.png';
+  static const String onBoarding2 = 'assets/images/onboarding2.png';
+  static const String onBoarding3 = 'assets/images/onboarding3.png';
+  static const String success = 'assets/images/Success.png';
+  static const String Vector1 = 'assets/images/Vector1.svg';
+  static const String Vector2 = 'assets/images/Vector2.svg';
+}
---
+++ lib/core/utilis/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+abstract class AppColors{
+  static const primaryColor = Color(0xFFC49E85);
+  static const offWhite = Color(0xFFF8F4F9);
+  static const deepBrown = Color(0xFF6B4B3E);
+  static const deepGrey = Color(0xFF333333);
+  static const lightGrey = Color(0xFFE5E5E5);
+  static const grey = Color(0xFFBDBDBD);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+
+}
